1eea1e366fb38f5e02663c6f2888c861c718a60f
skite <skite@mail.ithome.com.tw>
2022-03-07 21:08:38 +0800

    add NewTPELeague photo album link

--- js/media.js
+++ js/media.js
@@ -343,7 +343,17 @@ new Vue({
                 opponents: 'BT-Spirits、桃園自強衝鋒少年、森巴獵豹U10、twfc-豹、Vikings、長庚國小',
                 url: `<a target="_blank" href="https://photos.app.goo.gl/AwBDskj5a7yoU2Fc7"><div class="img-wrap image-container m-3 px-5"><img src="img/media/20220219-cover.jpg" class="img-fluid image"><div class="middle"><div class="text">前往相簿</div></div></div></a>`
             }],
-            icon: 'far fa-star'
+            icon: 'far fa-paw-claws'
+        }, {
+            id: 13,
+            cupName: '2022新北市少年八人制足球聯賽',
+            cover: '20220219-cover',
+            photos: [{
+                date: '3/6',
+                opponents: '台北曄拓',
+                url: `<a target="_blank" href="https://photos.app.goo.gl/CuuvTG5cbXuiA9uk8"><div class="img-wrap image-container m-3 px-5"><img src="img/media/20220219-cover.jpg" class="img-fluid image"><div class="middle"><div class="text">前往相簿</div></div></div></a>`
+            }],
+            icon: 'fas fa-running'
         }]
     },
     methods: {},
